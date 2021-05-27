721a60e8f655074a9c44d5596135b6062bd7c419
ahmar <ahmarkhanabro@gmail.com>
2021-05-27 20:38:04 +0500

    select prod for notification

--- lib/Pages/Vendor/Notifications/notification_page.dart
+++ lib/Pages/Vendor/Notifications/notification_page.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:fyp_smart_shopping/Pages/Vendor/select_item.dart';
 import 'package:fyp_smart_shopping/components/constants.dart';
 import 'package:fyp_smart_shopping/components/round_button.dart';
 import 'package:fyp_smart_shopping/Pages/Vendor/Products/vendor_products.dart';
+import 'package:fyp_smart_shopping/Pages/Vendor/Notifications/select_item.dart';
+
+String prodName = 'yes poop?';
+
 
 class notification extends StatefulWidget {
   static const String id = 'notifications';
   @override
-  _notificationState createState() => _notificationState();
+  _notificationState createState(){
+    return _notificationState();
+  }
 }
 
 class _notificationState extends State<notification> {
   @override
   Widget build(BuildContext context) {
+    String prodName2 = prodName;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.deepOrangeAccent,
@@ -24,11 +30,9 @@ class _notificationState extends State<notification> {
         body: Padding(
             padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 24.0),
           child: ListView(
-
             children: <Widget>[
               TextField(
                 onChanged: (value) {
-
                 },
                 decoration: kTextFieldDecoration.copyWith(hintText: 'Enter Notification Text'),
               ),
@@ -38,18 +42,24 @@ class _notificationState extends State<notification> {
                   onTap: () {
                     print('Card tapped.');
                   },
-                  child: const SizedBox(
+                  child: SizedBox(
                     width: 300,
                     height: 100,
-                    //child: ListTile(title: Text('Select Product to link'), onTap: Navigator.pushNamed(context, SelectItem.id)),
+                    child: ListTile(title: Text(prodName2),
+                    ),
                   ),
                 ),
               ),
               RoundButton(
-                title: "Select Product", onPressed: ()=> Navigator.pushNamed(context, VendorProducts.id),
+                title: "Select Product", onPressed: ()=> Navigator.pushNamed(context, SelectItem.id),
               ),
               RoundButton(
                 title: "Send for Approval",
+                onPressed:(){
+                  prodName2 = prodName;
+                  print("approval pressed");
+                  setState(() {});
+                },
               ),
             ],
           ),
---
+++ lib/Pages/Vendor/Notifications/show_products.dart
@@ -0,0 +1,77 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fyp_smart_shopping/Pages/Vendor/Products/led.dart';
+import 'package:fyp_smart_shopping/Pages/Vendor/Products/charger.dart';
+import 'package:fyp_smart_shopping/Pages/Vendor/Products/graphic_card.dart';
+import 'package:fyp_smart_shopping/Pages/Vendor/vendor_home.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:fyp_smart_shopping/Pages/Vendor/Notifications/notification_page.dart';
+
+class ShowProducts extends StatelessWidget {
+  FirebaseStorage _storage = FirebaseStorage.instance;
+  final String ve = VendorHome().getEmail();
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference products =
+    FirebaseFirestore.instance.collection('products');
+    return StreamBuilder<QuerySnapshot>(
+      stream: products.snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text('Something went wrong'),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: Text('Loading'),
+          );
+        }
+
+        return new ListView(
+          children: snapshot.data.docs.map((DocumentSnapshot document) {
+            if (document.data()['vendorEmail'] == ve) {
+              return inkwell(document, context);
+            } else {
+              return Container(
+                height: 0,
+                child: Text(''),
+              );
+            }
+          }).toList(),
+        );
+      },
+    );
+  }
+
+  InkWell inkwell(DocumentSnapshot document, BuildContext context) {
+    return InkWell(
+      onTap: () {
+        prodName = 'ahmarrr';
+        Navigator.pushNamed(context, notification.id);
+      },
+      child: new ListTile(
+        leading: document.data()['imageURL'] == null
+            ? Image.network(
+            'https://firebasestorage.googleapis.com/v0/b/smartshopping-979f9.appspot.com/o/images%2FNo_Image_Available.jpg?alt=media&token=d109c767-611b-4cf4-a55f-4e0ad304f4c5')
+            : Image.network(document.data()['imageURL']),
+        title: new Text(document.data()['title']),
+        subtitle: new Text(document.data()['price']),
+      ),
+    );
+  }
+
+  Future<void> deleteImage(String url, DocumentSnapshot d) async{
+    var photo = _storage.refFromURL(url);
+    await photo.delete();
+  }
+
+  void removeProduct(DocumentSnapshot d) {
+    print(d.data()['title']);
+    String imgFileURL = d["imageURL"];
+    deleteImage(imgFileURL, d);
+    d.reference.delete().then((value) => print("Product Deleted"))
+        .catchError((error) => print("Failed to delete product: $error"));
+  }
+}
