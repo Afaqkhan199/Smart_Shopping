78ea2ab9b545f7f40a6edcdea51175d2dc09f015
Afaq <afaqk380@gmail.com>
2021-07-14 08:19:30 +0500

    commit123

--- lib/Pages/Admin/Notification Approval/Orders/order_details.dart
+++ lib/Pages/Admin/Notification Approval/Orders/order_details.dart
@@ -15,6 +15,114 @@ class OrderDetails extends StatelessWidget {
           style: kAppBarTitleTextStyle,
         ),
       ),
+      body: ListView(
+        children: <Widget>[
+          Text(
+            "     Order ID: " + doc.data()['OrderID'],
+            style: TextStyle(
+              color: Colors.red,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            "     No. of Items: " + doc.data()['NoOfItems'],
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            "     Title: " + doc.data()['title'],
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            "     Price: " + doc.data()['price'],
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            "     Vendor Email: " + doc.data()['vendorEmail'],
+            style: TextStyle(
+              color: Colors.red,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            "     Vendor Address: " + doc.data()['VendorAddress'],
+            style: TextStyle(
+              color: Colors.red,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            "     Customer Email: " + doc.data()['customerEmail'],
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            "     Customer Address: " + doc.data()['CustomerAddress'],
+            style: TextStyle(
+              color: Colors.red,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            "     Order Time: " + doc.data()['OrderTime'],
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            "     Total Price: " + doc.data()['Total Price'],
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+        ],
+      ),
     );
   }
 }
